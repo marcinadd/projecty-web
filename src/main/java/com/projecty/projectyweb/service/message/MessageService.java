749403baf4906288da8426935d152a825916ad11
marcinadd <marcinadd@gmail.com>
2019-06-17 08:39:30 +0200

    MessageController cleanup

--- src/main/java/com/projecty/projectyweb/service/message/MessageService.java
+++ src/main/java/com/projecty/projectyweb/service/message/MessageService.java
@@ -1,4 +1,11 @@
 package com.projecty.projectyweb.service.message;
 
+import com.projecty.projectyweb.model.Message;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 public interface MessageService {
+    boolean checkIfCurrentUserHasPermissionToView(Message message);
+    void updateSeenDate(Message message);
+    void sendMessage(String recipientUsername, Message message, BindingResult bindingResult);
 }
