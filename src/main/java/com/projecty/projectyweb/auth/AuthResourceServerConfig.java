7d66fe3e46de224295f03ba4149251f95475801d
marcinadd <marcinadd@gmail.com>
2019-08-29 17:23:40 +0200

    Fix tests authorization error fix #21

--- src/main/java/com/projecty/projectyweb/auth/AuthResourceServerConfig.java
+++ src/main/java/com/projecty/projectyweb/auth/AuthResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.projecty.projectyweb.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -8,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 @Configuration
 @EnableResourceServer
+@Profile(value = {"development", "production"})
 public class AuthResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
