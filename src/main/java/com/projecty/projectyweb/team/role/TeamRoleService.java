c53162488ea13297e2b66ffa1834dc160f5b9d3e
Dida Ratsimba <dida.ratsimba@gmail.com>
2019-10-30 14:42:45 +0100

    add findById and delete into TeamRoleService.java

--- src/main/java/com/projecty/projectyweb/team/role/TeamRoleService.java
+++ src/main/java/com/projecty/projectyweb/team/role/TeamRoleService.java
@@ -26,14 +26,7 @@ public class TeamRoleService {
         if (usernames != null) {
             Set<User> users = userService.getUserSetByUsernamesWithoutCurrentUser(usernames);
             removeExistingUsersInTeamFromSet(users, team);
-            for (User user : users
-            ) {
-                TeamRole teamRole = new TeamRole();
-                teamRole.setTeam(team);
-                teamRole.setUser(user);
-                teamRole.setName(TeamRoles.MEMBER);
-                teamRoles.add(teamRole);
-            }
+            users.forEach(user -> teamRoles.add(new TeamRole(TeamRoles.MEMBER, user, team)));
         }
         if (team.getTeamRoles() == null) {
             team.setTeamRoles(teamRoles);
@@ -44,10 +37,7 @@ public class TeamRoleService {
 
     public void addCurrentUserAsTeamManager(Team team) {
         User current = userService.getCurrentUser();
-        TeamRole teamRole = new TeamRole();
-        teamRole.setTeam(team);
-        teamRole.setName(TeamRoles.MANAGER);
-        teamRole.setUser(current);
+        TeamRole teamRole = new TeamRole(TeamRoles.MANAGER, current, team);
         if (team.getTeamRoles() == null) {
             List<TeamRole> teamRoles = new ArrayList<>();
             teamRoles.add(teamRole);
@@ -71,10 +61,7 @@ public class TeamRoleService {
     public Set<User> getTeamRoleUsers(Team team) {
         List<TeamRole> teamRoles = teamRoleRepository.findByTeam(team);
         Set<User> users = new HashSet<>();
-        for (TeamRole teamRole : teamRoles
-        ) {
-            users.add(teamRole.getUser());
-        }
+        teamRoles.forEach(teamRole -> users.add(teamRole.getUser()));
         return users;
     }
 
@@ -90,14 +77,25 @@ public class TeamRoleService {
         teamRoleRepository.save(teamRole);
     }
 
+    private int getTeamManagersCount(Team team) {
+        List<TeamRole> teamRoles = team.getTeamRoles();
+        int managers = 0;
+        for (TeamRole teamRole : teamRoles
+        ) {
+            if (teamRole.getName().equals(TeamRoles.MANAGER)) {
+                managers++;
+            }
+        }
+        return managers;
+    }
+
     public List<TeamRole> getTeamRolesWhereManager(User user) {
         List<TeamRole> managerTeamRoles = new ArrayList<>();
-        for (TeamRole teamRole : user.getTeamRoles()
-        ) {
+        user.getTeamRoles().forEach(teamRole -> {
             if (teamRole.getName() == TeamRoles.MANAGER) {
                 managerTeamRoles.add(teamRole);
             }
-        }
+        });
         return managerTeamRoles;
     }
 
@@ -105,10 +103,19 @@ public class TeamRoleService {
         Optional<TeamRole> optionalTeamRole = teamRoleRepository.findByTeamAndAndUser(team, user);
         if (optionalTeamRole.isPresent()) {
             team.getTeamRoles().remove(optionalTeamRole.get());
-            if (teamRoleRepository.countByTeamAndName(team, TeamRoles.MANAGER) == 0) {
+            if (getTeamManagersCount(team) == 0) {
                 throw new NoManagersInTeamException();
             }
             teamRepository.save(team);
         }
     }
+
+	public Optional<TeamRole> findById(Long teamRoleId) {
+		return teamRoleRepository.findById(teamRoleId);
+	}
+
+	public void delete(TeamRole teamRole) {
+		teamRoleRepository.delete(teamRole);
+	}
+    
 }
